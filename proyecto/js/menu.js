5db71fdd35ea0dbad6dd36da1f12ca41b2b72673
frank272208 <camposfranklin927@gmail.com>
2021-09-01 02:29:20 -0400

    sdfs

--- proyecto/js/menu.js
+++ proyecto/js/menu.js
@@ -1,5 +1,23 @@
-function menudesplegable()
-{
-    document.getElementsByClassName
-    ("navigation")[0].classList.toggle("responsive");
-}
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
